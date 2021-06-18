08409c6adbe58eaf53c000d5b5bbae632cb61100
kiocam <jasoncameron@Jasons-MacBook-Pro.local>
2021-06-17 22:00:03 -0400

    initital project setup and config

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <core-drawer />
+
+    <v-main class="overflow-hidden">
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    components: {
+      CoreDrawer: () => import('@/components/core/Drawer'),
+    },
+  }
+</script>
+
+<style>
+  .text-decoration-none {
+    text-decoration: none;
+  }
+
+  .profile-img {
+      width: 450px;
+      right: unset !important;
+      top: 332px !important;
+    }
+
+    .v-timeline {
+      z-index: 5;
+    }
+</style>
---
+++ src/views/WhatIveDone.vue
@@ -0,0 +1,56 @@
+<template>
+  <core-section id="what-ive-done">
+    <abstract-3 />
+
+    <core-heading>What I've done</core-heading>
+
+    <v-col cols="12">
+      <v-carousel
+        :cycle="false"
+        :height="650"
+        :show-arrows="false"
+        class="elevation-0"
+        hide-delimiter-background
+      >
+        <v-carousel-item
+          v-for="(project, i) in projects"
+          :key="i"
+        >
+          <project :value="project" />
+        </v-carousel-item>
+      </v-carousel>
+    </v-col>
+  </core-section>
+</template>
+
+<script>
+  export default {
+    name: 'WhatIveDone',
+
+    components: {
+      Abstract3: () => import('@/components/Abstract3'),
+      Project: () => import('@/components/Project'),
+    },
+
+    data: () => ({
+      projects: [
+        {
+          name: 'Glory-MS',
+          category: 'Web development / backend api development',
+          src: 'gloryms.png',
+          date: 'September, 2020',
+          tech: 'Vue.js, node.js, StrapiCms, Google Firebase',
+          projectInfo: 'Design and devloped a web platform for a private gaming community, where users can register for gaming accounts, view account details via dashboard etc.',
+        },
+        {
+          name: 'Initium Digital',
+          category: 'Web Development',
+          src: 'initium.png',
+          date: 'april, 2020',
+          tech: 'Vue.js, Sanity.io, Netlify',
+          projectInfo: 'Developed a platform for an upcomming Digital studio, & provided hosting',
+        },
+      ],
+    }),
+  }
+</script>
